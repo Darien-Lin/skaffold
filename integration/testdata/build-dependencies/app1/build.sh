7124682a08432f567b7f15aec6c4a470fba0d527
Minna Howell <minnah@google.com>
2026-02-17 20:58:11 +0000

    more fixes

--- integration/testdata/build-dependencies/app1/build.sh
+++ integration/testdata/build-dependencies/app1/build.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -e
+ARGS=""
 # build arg image2 is set by Skaffold to be the image built for app2
-docker build -t "$IMAGE" --build-arg image2 .
 if [[ "${PUSH_IMAGE}" == "true" ]]; then
-    docker push "$IMAGE"
+ARGS="--push"
+else
+ARGS="--load"
 fi
+docker buildx build $ARGS -t "$IMAGE" --build-arg image2 .
